e09a2c81fce767af18ac4bc621c118f33de87cc8
gurgenidzegiorgi <gurgenidzegiorgi69@gmail.com>
2023-05-29 18:31:55 -0400

    add project main content files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import AccordionCard from "./components/AccordionCard/AccordionCard";
+
+function App() {
+  return (
+    <div className="app">
+      <AccordionCard />
+    </div>
+  );
+}
+
+export default App;
